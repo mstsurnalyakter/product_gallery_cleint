ae6323724da99b5474b3cac62648dee740fb5095
mstsurnalyakter <surnalyms@gmail.com>
2024-11-18 13:50:48 +0600

    layout setup

---
+++ src/page/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Home = () => {
+  return (
+    <div className="space-y-20 dark:text-gray-100">
+      <h1>HomGGGGGGGGe</h1>
+    </div>
+  );
+};
+
+export default Home;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main";
+import Home from "../page/Home/Home";
 
 
 const router = createBrowserRouter([
@@ -9,7 +10,7 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: "Home",
+        element: <Home/>,
       },
 
     ],
